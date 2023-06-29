fe69cc7f57d54a98b163f523c388ad1b573b600b
Manoj Saini <manojsaini513@gmail.com>
2023-06-29 10:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function generatePromise(){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world")
+		}, 1000)
+	})
+}
+
+generatePromise().then((data) => {
+	const ele = document.getElementById("output");
+	ele.innerText = data;
+})
